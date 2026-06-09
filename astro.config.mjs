57749bccfb37a48cdfe13612d756ef028dd60e7f
wai46704 <tom.b770103@gmail.com>
2026-06-09 18:01:28 +0900

    feat: サイトマップとrobots.txtを追加（インデックス登録対策）

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
   site: 'https://naoki-blog.com',
+  integrations: [sitemap()],
 });
